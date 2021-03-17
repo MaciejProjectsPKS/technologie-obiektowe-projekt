b35b643d1ddda7a4d618f6ff4ecf330a0f352892
LAzarrr22 <maciek.janik24@gmail.com>
2021-03-17 17:20:03 +0100

    application

---
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/ConverterMaciejJanikApplication.java
@@ -0,0 +1,13 @@
+package com.MJ.converterMaciejJanik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterMaciejJanikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterMaciejJanikApplication.class, args);
+	}
+
+}
---
+++ APP/converterMaciejJanik/src/test/java/com/MJ/converterMaciejJanik/ConverterMaciejJanikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MJ.converterMaciejJanik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterMaciejJanikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
