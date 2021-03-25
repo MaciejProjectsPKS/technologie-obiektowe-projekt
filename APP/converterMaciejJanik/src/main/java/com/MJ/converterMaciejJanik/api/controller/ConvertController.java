9045ba60c7211b8e9a2df33e9ea4c34695f5b0b2
LAzarrr22 <maciek.janik24@gmail.com>
2021-03-25 18:08:04 +0100

    add methods to convert data and model for test

--- APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/controller/ConvertController.java
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/controller/ConvertController.java
@@ -1,16 +1,36 @@
 package com.MJ.converterMaciejJanik.api.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.MJ.converterMaciejJanik.api.services.ConvertAPIService;
+import com.MJ.converterMaciejJanik.models.enums.Extension;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/converter")
+@AllArgsConstructor
 public class ConvertController {
+
+    @Autowired
+    private ConvertAPIService convertAPIService;
 
     @RequestMapping(path = "/hello", method = RequestMethod.GET, produces = "application/json")
     public String test(){
         return "Hello World";
+    }
+
+    @RequestMapping(path = "/content/{sourceEXT}/to/{targetEXT}", method = RequestMethod.POST, produces = "application/json")
+    public Object Convert(@PathVariable Extension sourceEXT, @PathVariable Extension targetEXT,
+                          @RequestBody String content, @RequestParam(name="save") String fileSavePath){
+
+        return convertAPIService.convertBodyContent(content, sourceEXT, targetEXT, fileSavePath);
+    }
+
+    @RequestMapping(path = "/file/to/{targetEXT}", method = RequestMethod.POST, produces = "application/json")
+    public Object ConvertFromFile( @PathVariable Extension targetEXT, @RequestParam(name="path") String filePath,
+                                   @RequestParam(name="save") String fileSavePath){
+
+        return convertAPIService.convertFileContent(filePath, targetEXT, fileSavePath);
     }
 }
