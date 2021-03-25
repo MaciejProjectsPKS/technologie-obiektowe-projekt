e5385692c46b2d146ceb6cd0500625c3895d281a
LAzarrr22 <maciek.janik24@gmail.com>
2021-03-25 18:07:32 +0100

    add endpoints to execute conversion

--- APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/services/ConvertAPIService.java
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/services/ConvertAPIService.java
@@ -1,7 +1,17 @@
 package com.MJ.converterMaciejJanik.api.services;
 
+import com.MJ.converterMaciejJanik.models.enums.Extension;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConvertAPIService {
+
+
+    public Object convertBodyContent(String content, Extension sourceEXT, Extension targetEXT, String fileSavePath) {
+        return new TestModel(content,sourceEXT,targetEXT,fileSavePath);
+    }
+
+    public Object convertFileContent(String filePath, Extension targetEXT, String fileSavePath) {
+        return new TestModel("filePath: "+filePath,null,targetEXT,fileSavePath);
+    }
 }
