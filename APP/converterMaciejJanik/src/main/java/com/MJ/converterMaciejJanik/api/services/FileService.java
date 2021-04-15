1c593b693a4a70954c593e9d3bc27bb5f323ee04
LAzarrr22 <maciek.janik24@gmail.com>
2021-04-15 19:39:10 +0200

    poprawka rozbudowanie file readera - czytanie każdej linii

--- APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/services/FileService.java
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/services/FileService.java
@@ -2,7 +2,9 @@ package com.MJ.converterMaciejJanik.api.services;
 
 import com.MJ.converterMaciejJanik.models.enums.Extension;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.*;
 
@@ -11,18 +13,26 @@ import java.io.*;
 public class FileService {
 
     public String readFile(String path){
+        StringBuilder content= new StringBuilder();
         try {
             File file = new File(path);
             FileReader fileReader = new FileReader(file);
             BufferedReader br = new BufferedReader(fileReader);
-            return br.readLine();
+
+            String line;
+            while((line = br.readLine()) != null){
+                content.append(line);
+            }
+            br.close();
+            fileReader.close();
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return "Error";
+        return content.toString();
     }
 
 
