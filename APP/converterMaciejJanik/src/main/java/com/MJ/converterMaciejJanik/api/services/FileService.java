b43ad67371f8fca33fcf606458cbf6aabd297879
LAzarrr22 <maciek.janik24@gmail.com>
2021-04-15 19:26:32 +0200

    dodanie metody obsługującej czytanie pliku z podanej lokalizacji

--- APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/services/FileService.java
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/api/services/FileService.java
@@ -3,11 +3,24 @@ package com.MJ.converterMaciejJanik.api.services;
 import com.MJ.converterMaciejJanik.models.enums.Extension;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+
 @Service
 public class FileService {
 
     public String readFile(String path){
-        return "test";
+        try {
+            File file = new File(path);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader br = new BufferedReader(fileReader);
+            return br.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Error";
     }
 
 
