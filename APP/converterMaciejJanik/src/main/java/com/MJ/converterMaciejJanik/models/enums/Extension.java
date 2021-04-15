1286b41707d0d089348a332d8848ff6c64975a57
LAzarrr22 <maciek.janik24@gmail.com>
2021-04-15 19:25:53 +0200

    dodanie domyślnego typu pliku i jego obsługa

--- APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/models/enums/Extension.java
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/models/enums/Extension.java
@@ -1,6 +1,23 @@
 package com.MJ.converterMaciejJanik.models.enums;
 
+import java.util.Arrays;
+
 public enum Extension {
 
-    JSON, XML, YML
+    JSON("json"),
+    XML("xml"),
+    YML("yml"),
+    INVALID("invalid");
+
+
+    Extension(String code) {
+        this.code =code;
+    }
+    private final String code;
+
+
+    public static Extension fromString(String value){
+        return Arrays.stream(Extension.values()).filter(s -> s.code.equalsIgnoreCase(value)).findFirst()
+                .orElse(Extension.INVALID);
+    }
 }
