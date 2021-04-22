bf3fc15caba191e1ab45d1e875175bee3e51561b
LAzarrr22 <maciek.janik24@gmail.com>
2021-04-22 21:57:48 +0200

    configuration of Converter

---
+++ APP/converterMaciejJanik/src/main/java/com/MJ/converterMaciejJanik/converter/config/ConverterConfig.java
@@ -0,0 +1,15 @@
+package com.MJ.converterMaciejJanik.converter.config;
+
+public class ConverterConfig {
+
+    public static final int MAX_SEPARATOR_VALUE_SPACEBARS = 30;
+    public static final int MAX_SEPARATOR_COMMENT_SPACEBARS = 40;
+
+    public static final String DEFINE = "define ";
+    public static final String DEFINITION_START = " {";
+    public static final String DEFINITION_END = "}\n";
+    public static final String PROPERTY_INDENT = "    ";
+    public static final String INNER_COMMENT_INDENT = " ";
+
+    public static final String SERVICE_DEPENDENCIES_FILENAME = "servicedependencies.cfg";
+}
